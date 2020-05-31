144a560bbf2c6159c8f546dd9922facdd047995f
tykowale <tykowale@gmail.com>
2020-05-31 11:41:53 -0700

    FS: working communication between fe and be

---
+++ aruba-be/src/main/java/com/tykowale/aruba/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.tykowale.aruba.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Restrict this when there is good reason to but for now allow all CORS
+        registry.addMapping("/**");
+    }
+}
